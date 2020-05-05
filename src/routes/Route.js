1faca71c3d6be79b3a0da9c8492041aab6e2aea2
Juan Mello <juanribeirofx@gmail.com>
2020-05-05 19:55:41 -0300

    Utilizando Root Import

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
-import AuthLayout from '../pages/_layouts/auth';
-import DefaultLayout from '../pages/_layouts/default';
+import AuthLayout from '~/pages/_layouts/auth';
+import DefaultLayout from '~/pages/_layouts/default';
 
 export default function RouteWrapper({
   component: Component,
@@ -25,7 +25,7 @@ export default function RouteWrapper({
   return (
     <Route
       {...rest}
-      render={ props => (
+      render={(props) => (
         <Layout>
           <Component {...props} />
         </Layout>
