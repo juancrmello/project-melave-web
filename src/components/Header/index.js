cb38065484d31785d792fc0492127d6fb28cbee7
Juan Mello <juanribeirofx@gmail.com>
2020-05-26 18:23:08 -0300

    Configurando Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { Container, Content, Profile } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src="" alt="" />
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+
+        <aside>
+          <Profile>
+            <div>
+              <strong>Juan Mello</strong>
+              <Link to="/profile">Meu perfil</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/285/abott@adorable.png"
+              alt="Juan Mello"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
--- src/pages/_layouts/default/styles.js
+++ src/pages/_layouts/default/styles.js
@@ -2,5 +2,5 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   height: 100%;
-  background: #333;
+  background: linear-gradient(-90deg, #3fc1c9, #364f6b);
 `;
